e979d7c45be1cde0dba13bb60cbcebf098691f71
Jakub <gawlikjakub9@gmail.com>
2024-01-12 12:41:14 +0100

    adding order and relations

---
+++ src/main/java/pl/zajecia/backendproject/shop/model/Order.java
@@ -0,0 +1,40 @@
+package pl.zajecia.backendproject.shop.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime orderDate;
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+    @OneToMany(mappedBy = "order")
+    private Set<OrderItem> orderItems = new HashSet<>();
+
+    public Order(Client client) {
+        this.client = client;
+        this.orderDate = LocalDateTime.now();
+    }
+
+}
