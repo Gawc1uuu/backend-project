4065e34f13dfd94b1c7b6012cd54a82da7900e8e
majki <m.czarnota376@gmail.com>
2024-01-12 11:13:48 +0100

    Adding product entity

---
+++ src/main/java/pl/zajecia/backendproject/shop/model/Product.java
@@ -0,0 +1,22 @@
+package pl.zajecia.backendproject.shop.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private Long price;
+    private Long quantity;
+}
