e7c907ca948d0f5866e39e28bf9da10033892236
majki <m.czarnota376@gmail.com>
2024-01-17 18:22:54 +0100

    Creating routes for getting all products from database, also creating methods to getting single product by ID and editing them by ID

--- src/main/java/pl/zajecia/backendproject/shop/service/ProductService.java
+++ src/main/java/pl/zajecia/backendproject/shop/service/ProductService.java
@@ -1,10 +1,14 @@
 package pl.zajecia.backendproject.shop.service;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import pl.zajecia.backendproject.shop.model.Product;
 import pl.zajecia.backendproject.shop.model.command.ProductCommand;
 import pl.zajecia.backendproject.shop.repository.ProductRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +21,23 @@ public class ProductService {
 
         repository.saveAndFlush(product);
 
+    }
+
+    public Product findById(Long id){
+        return repository.findById(id).orElseThrow(()->new IllegalArgumentException("cannot find product"));
+    }
+
+    public Product updateProduct(Long id, ProductCommand command){
+       Product product = repository.findById(id).orElseThrow(()->new IllegalArgumentException("cannot find product"));
+       product.setName(command.getName());
+       product.setPrice(command.getPrice());
+       product.setQuantity(command.getQuantity());
+       repository.saveAndFlush(product);
+       return product;
+    }
+
+    public List<Product> gettingProducts(){
+        List<Product> products = repository.findAll();
+        return products;
     }
 }
